6a0e85a0590b1b4a205c8366ea1689a89fcde57f
Vitor Wunsch <vitor.wunsch@outlook.com>
2021-04-20 20:05:01 -0300

    first class, beginning

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+
+const app = express();
+
+app.get("/", (request, response) =>{
+    return response.json({
+        message: "Hello NLW 05"
+    });
+});
+
+app.post("/users", (request, response) => {
+    return response.json({
+        message: "User saved with success"
+    });
+});
+
+app.listen(3333, () => console.log('Server is running on port 3333'));  
