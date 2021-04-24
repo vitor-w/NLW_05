3d6ca95eb2f70da9c3fbf6477b139936934bf49f
Vitor Wunsch <vitor.wunsch@outlook.com>
2021-04-23 23:02:26 -0300

    Fourth class

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -26,10 +26,20 @@ class UsersService {
         const user = this.usersRepository.create({
             email
         });
+
         await this.usersRepository.save(user);
 
         return user;
-    }
+    };
+
+    async findByEmail(email: string) {
+
+        const user = await this.usersRepository.findOne({
+            email
+        });
+
+        return user;
+    };
 }
 
 export { UsersService };
